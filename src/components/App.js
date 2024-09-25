6c9d0e02e07121415cb16552e3d637492939b38c
irfan-007 <thanatos786687@gmail.com>
2024-09-25 13:45:00 +0530

    done 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const dataFetcher = () => {
+      let data = fetch(" https://dummyjson.com/products")
+        .then((data) => data.json())
+        .then((json) => {
+          setData(json);
+          console.log(json);
+        });
+    };
+    dataFetcher();
+  }, []);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <pre>{data ? JSON.stringify(data, undefined, 4) : "Loading..."}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
