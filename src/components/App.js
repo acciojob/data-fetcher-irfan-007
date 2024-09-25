59392fbfca794035299f3cb89a8a61eb0527db7a
irfan-007 <thanatos786687@gmail.com>
2024-09-25 15:21:20 +0530

    done 3

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import "./../styles/App.css";
 
 const App = () => {
   const [data, setData] = useState();
+  const [myerr, setMyerr] = useState(null);
   useEffect(() => {
     const dataFetcher = () => {
       let data = fetch(" https://dummyjson.com/products")
@@ -14,12 +15,16 @@ const App = () => {
           // console.log(json);
           setData(json);
         })
-        .catch((err) => console.error("An error occurred: ", err));
+        .catch((err) => {
+          console.error("An error occurred: ", err);
+          setMyerr("An error occurred: ");
+        });
     };
     dataFetcher();
   }, []);
   return (
     <div id="main">
+      {myerr && myerr}
       <h1>Data Fetched from API</h1>
       <pre>{data ? JSON.stringify(data, undefined, 2) : "Loading..."}</pre>
     </div>
